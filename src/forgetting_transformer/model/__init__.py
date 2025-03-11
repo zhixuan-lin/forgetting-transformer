8e056e7a71eff5652005da79d91c9c6e5a0ab465
Zhixuan Lin <zxlin.cs@gmail.com>
2025-03-11 18:58:53 -0400

    Move HF registering code to model/__init__.py

--- src/forgetting_transformer/model/__init__.py
+++ src/forgetting_transformer/model/__init__.py
@@ -5,10 +5,19 @@
 # from .hgrn2 import HGRN2ForCausalLM, HGRN2Config
 # from .samba import SambaForCausalLM, SambaConfig
 
-from forgetting_transformer.model.forgetting_transformer import (
-    ForgettingTransformerForCausalLM,
-    ForgettingTransformerConfig,
-)
-from forgetting_transformer.model.forgetting_transformer.modeling_forgetting_transformer import (
-    ForgettingAttentionLayer
-)
+import importlib
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings(action="ignore", message="Flash Attention is not installed")
+    warnings.filterwarnings(action="ignore", message="`torch.cuda.amp")
+    from forgetting_transformer.model.forgetting_transformer import (
+        ForgettingTransformerForCausalLM,
+        ForgettingTransformerConfig,
+    )
+    from forgetting_transformer.model.forgetting_transformer.modeling_forgetting_transformer import (
+        ForgettingAttentionLayer
+    )
+
+    for model in ["mamba2", "forgetting_transformer", "transformer", "delta_net", "hgrn2", "samba"]:
+        # We do not want to espose the names.
+            importlib.import_module(f".{model}", __name__)
--- src/forgetting_transformer/model/register_all.py
+++
@@ -1,6 +0,0 @@
-from . import mamba2
-from . import forgetting_transformer
-from . import transformer
-from . import delta_net
-from . import hgrn2
-from . import samba
