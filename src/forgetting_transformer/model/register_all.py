5149e08b6efbf2fe817928556d9f002589580fc8
Zhixuan Lin <zxlin.cs@gmail.com>
2025-08-11 10:33:47 -0400

    Fix registering bug

--- src/forgetting_transformer/model/register_all.py
+++ src/forgetting_transformer/model/register_all.py
@@ -5,4 +5,4 @@ with warnings.catch_warnings():
     warnings.filterwarnings(action="ignore", message="`torch.cuda.amp")
     for model in ["mamba2", "forgetting_transformer", "transformer", "delta_net", "hgrn2", "samba"]:
         # We do not want to espose the names.
-            importlib.import_module(f".{model}", __name__)
+            importlib.import_module(f".{model}", __package__)
